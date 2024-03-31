964dd940b0cd42f296f898944765b1d21c1ec299
Nima Dorji Moktan <moktannima2015@gmail.com>
2024-03-31 20:40:05 +0800

    Files updated

--- myPage/settings.py
+++ myPage/settings.py
@@ -124,6 +124,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Settings to incorporate global static files
+STATICFILES_DIRS = [
+    # BASE_DIR / "static"
+    "mypage/static"
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
